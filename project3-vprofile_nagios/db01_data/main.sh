b8fdebc847b55dd332c06abaa548f52f6bb6ec74
saleh1312 <profsoft1312@gmail.com>
2025-01-24 16:34:32 +0200

    s

--- project3-vprofile_nagios/db01_data/main.sh
+++ project3-vprofile_nagios/db01_data/main.sh
@@ -22,7 +22,7 @@ systemctl restart mariadb
 
 systemctl start firewalld
 systemctl enable firewalld
-firewall-cmd--get-active-zones
-firewall-cmd--zone=public--add-port=3306/tcp--permanent
-firewall-cmd--reload
+firewall-cmd --get-active-zones
+firewall-cmd --zone=public --add-port=3306/tcp --permanent
+firewall-cmd --reload
 systemctl restart mariadb
