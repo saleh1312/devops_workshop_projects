cdb6d4eb19ae1129c9bb5a01974bb5e0851482b6
saleh1312 <profsoft1312@gmail.com>
2025-02-20 03:14:04 +0200

    s

--- project1-vprofile/web02_data/main.sh
+++ project1-vprofile/web02_data/main.sh
@@ -1,5 +1,10 @@
 sudo dnf install nginx -y
 
-sudo cp /vagrant/web02_data/vproapp.conf /etc/nginx/conf.d/vproapp.conf
+sudo cp /vagrant/web02_data/vproapp.conf /etc/nginx/sites-available/vproapp
+
+rm -rf /etc/nginx/sites-enabled/default
+
+ln -s /etc/nginx/sites-available/vproapp /etc/nginx/sites-enabled/vproapp
 
 sudo systemctl restart nginx
+
